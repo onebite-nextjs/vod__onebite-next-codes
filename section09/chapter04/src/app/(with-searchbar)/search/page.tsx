27c51f3f739544ab1a70c2edd7b9bea2ad9182ac
winterlood <king199777@gmail.com>
2025-04-07 13:33:07 +0900

    INITIAL

---
+++ section09/chapter04/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,51 @@
+import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import { BookData } from "@/types";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+async function SearchResult({ q }: { q: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+type Props = {
+  searchParams: Promise<{ q?: string }>;
+};
+
+export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
+  const { q } = await searchParams;
+  return {
+    title: `${q} : 한입북스 검색`,
+    description: `${q} 검색 결과입니다`,
+    openGraph: {
+      title: `${q} : 한입북스 검색`,
+      description: `${q} 검색 결과입니다`,
+      images: ["/thumbnail.png"],
+    },
+  };
+}
+
+export default async function Page({ searchParams }: Props) {
+  const { q } = await searchParams;
+  return (
+    <Suspense key={q || ""} fallback={<BookListSkeleton count={3} />}>
+      <SearchResult q={q || ""} />
+    </Suspense>
+  );
+}
